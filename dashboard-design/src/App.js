24a3e2bd36d8c6a8ad0c114fe6f5f0e25128d975
Sick-Carlito <carlb1319@gmail.com>
2020-10-01 02:44:25 +0300

    Basic layout

--- dashboard-design/src/App.js
+++ dashboard-design/src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+
+// import { Button } from 'react-bulma-components';
+import { Columns } from 'react-bulma-components';
+import { Card } from 'react-bulma-components';
+import { Content } from 'react-bulma-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>ah niaje msee!! </p>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="Body">
+    <Columns>
+      <Columns.Column size={3.5}>
+      <Card className='Card'>
+      
+      <Card.Content>
+        
+        <Content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          {/* <a href="#1">#css</a> <a href="#2">#responsive</a> */}
+          <br />
+          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </Content>
+      </Card.Content>
+    </Card>
+      </Columns.Column>
+    
+    <Columns.Column size={5}>
+    <Card className='Card'>
+      
+      <Card.Content>
+        
+        <Content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          {/* <a href="#1">#css</a> <a href="#2">#responsive</a> */}
+          <br />
+          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </Content>
+      </Card.Content>
+    </Card>
+      </Columns.Column>
+
+      <Columns.Column size={3.5}>
+      <Card className='Card'>
+      
+      <Card.Content>
+        
+        <Content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          {/* <a href="#1">#css</a> <a href="#2">#responsive</a> */}
+          <br />
+          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </Content>
+      </Card.Content>
+    </Card>
+      </Columns.Column>
+
+      </Columns>
+    {/* </div> */}
+</div>
+  
+
+    
   );
 }
 
