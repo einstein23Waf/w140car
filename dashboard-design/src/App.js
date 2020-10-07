b34b080ed32f272e26998bd79da367f4b734b78d
Sick-Carlito <carlb1319@gmail.com>
2020-10-08 01:14:29 +0300

    complete UI layout for customer dashboard

--- dashboard-design/src/App.js
+++ dashboard-design/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
+import Avatar from 'react-avatar';
 
-// import { Button } from 'react-bulma-components';
+import { Button } from 'react-bulma-components';
 import { Columns } from 'react-bulma-components';
 import { Card } from 'react-bulma-components';
 import { Content } from 'react-bulma-components';
 import { Container } from 'react-bulma-components';
-import { List } from 'react-bulma-components';
-import { Box } from 'react-bulma-components';
+// import { List } from 'react-bulma-components';
+// import { Box } from 'react-bulma-components';
+import { RiDashboardLine } from "react-icons/ri";
+import { FaSistrix } from "react-icons/fa";
+import { GiCamel} from "react-icons/gi";
 
 function App() {
 
@@ -23,38 +27,60 @@ function App() {
   return (
     <div className="Body">
       <Container className='Container'>
-    <Columns>
-      <Columns.Column size={2.5}>
+    <Columns className='columns'>
+    <Columns.Column size={12}>
+      <Columns>
+      <Columns.Column size={6} className='div1'>
+         <GiCamel className='camel' size='40px'/>
+      </Columns.Column>
+      <Columns.Column size={6} className='div2'>
+      
+         
+          <FaSistrix className='search' size='30px'/>
+          <FaSistrix className='search' size='30px'/>
+          <FaSistrix className='search' size='30px'/>
+      
+      </Columns.Column>
+      </Columns>
+      
+    </Columns.Column>
+      <Columns.Column size={3}>
       <Card className='Card'>
       
       <Card.Content>
 
-        <p><b>Transporter 1</b></p>
+        <p><b>Customer 1</b></p>
         <hr></hr>
       
       <ul onMouseOver={changeBackground} onMouseOut={changeBackgroundBack} >
-          <li className='li-item'>Dashboard</li>
-          <li className='li-item'>My Business Info</li>
-          <li className='li-item'>Drivers Manager</li>
-          <li className='li-item'>Vehicles Manager</li>
-          <li className='li-item'>Jobs Manager</li>
-          <li className='li-item'>Calender</li>
-          <li className='li-item'>Messages</li>
-          <li className='li-item'>Payments</li>
+          <li className='li-item'><RiDashboardLine className="icon"/>Dashboard</li>
+          <li className='li-item'><RiDashboardLine className="icon"/>User Profile</li>
+          <li className='li-item'><RiDashboardLine className="icon"/>Messages</li>
+          <li className='li-item'><RiDashboardLine className="icon"/>Payments</li>
+          <li className='li-item'><RiDashboardLine className="icon"/>Payments</li>
+          <li className='li-item'><RiDashboardLine className="icon"/>Payments</li>
+          <li className='li-item'><RiDashboardLine className="icon"/>Payments</li>
+          <li className='li-item'><RiDashboardLine className="icon"/>Payments</li>
       </ul>
+
+      <div className='line'>
+      <hr></hr>
+      </div>
+      
+      <Button color="primary" size="small" rounded outlined>Logout</Button>
     
       </Card.Content>
     </Card>
       </Columns.Column>
     
-    <Columns.Column size={7}>
+    <Columns.Column size={6}>
     <Card className='Card'>
       
       <Card.Content>
         
         <Content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+        <heading><b>Lead sales</b></heading>
+          <hr></hr>
           {/* <a href="#1">#css</a> <a href="#2">#responsive</a> */}
           <br />
           <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
@@ -68,6 +94,8 @@ function App() {
       <Card.Content>
         
         <Content>
+          <heading><b>Activity</b></heading>
+          <hr></hr>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Phasellus nec iaculis mauris. <a>@bulmaio</a>.
           {/* <a href="#1">#css</a> <a href="#2">#responsive</a> */}
@@ -84,6 +112,8 @@ function App() {
       <Card.Content>
         
         <Content>
+        <heading><b>Notifications</b></heading>
+          <hr></hr>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Phasellus nec iaculis mauris. <a>@bulmaio</a>.
           {/* <a href="#1">#css</a> <a href="#2">#responsive</a> */}
@@ -99,7 +129,10 @@ function App() {
 
       </Columns.Column>
 
-      <Columns.Column size={2.5}>
+      <Columns.Column size={3}>
+      
+      
+      
       <Card className='Card'>
       
       <Card.Content>
@@ -108,14 +141,18 @@ function App() {
         <hr></hr>
       
       <ul onMouseOver={changeBackground} onMouseOut={changeBackgroundBack} >
-          <li className='li-item'>Carl<time className='time' >11:09 PM</time></li>
-          <li className='li-item'>Carl<time className='time' >11:09 PM</time></li>
-          <li className='li-item'>Carl<time className='time' >11:09 PM</time></li>
-          <li className='li-item'>Carl<time className='time' >11:09 PM</time></li>
-          <li className='li-item'>Carl<time className='time' >11:09 PM</time></li>
-          <li className='li-item'>Carl<time className='time' >11:09 PM</time></li>
-          <li className='li-item'>Carl<time className='time' >11:09 PM</time></li>
-          <li className='li-item'>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
+          <li className='li-item'><Avatar size="40" round="35px" name="carl" className='Avatar'/>Carl<time className='time' >11:09 PM</time></li>
       </ul>
     
       </Card.Content>
